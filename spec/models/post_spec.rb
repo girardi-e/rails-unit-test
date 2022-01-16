662b12f0d87382329c4d23a9dc59fb5f5ea8d42d
emilio girardi <emilio.g.girardi@gmail.com>
2022-01-16 13:43:32 +0100

    code refactoring

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   # creating a user for the validation tests
-  current_user = User.first_or_create!(email: 'test@example.com', password: 'password', password_confirmation: 'password')
+  current_user = User.first_or_create!(
+    email: 'test@example.com',
+    password: 'password',
+    password_confirmation: 'password')
 
   # validation tests
   it 'cannot have an empty title' do
@@ -18,7 +21,7 @@ RSpec.describe Post, type: :model do
     expect(post).to be_valid
   end
 
-  it 'has a title at least two characters' do
+  it 'has a title at least two characters long' do
     post = Post.new(
       title: 'I',
       body: 'A valid body',
@@ -43,7 +46,6 @@ RSpec.describe Post, type: :model do
     post.body = 'A valid body'
     expect(post).to be_valid
   end
-
   it 'has a body at least 5 characters long' do
     post = Post.new(
       title: 'A valid title',
@@ -60,7 +62,8 @@ RSpec.describe Post, type: :model do
   it 'has a body at most 100 characters long' do
     post = Post.new(
       title: 'A valid title',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+             eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       views: 0,
       user: current_user
     )
